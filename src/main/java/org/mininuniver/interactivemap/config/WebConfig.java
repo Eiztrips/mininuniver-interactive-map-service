fd7c7300c4bbb52669d80844ec20901c4788c48d
Eiztrips <eiztrips@gmail.com>
2025-04-15 20:27:45 +0300

    Add auth

--- src/main/java/org/mininuniver/interactivemap/config/WebConfig.java
+++ src/main/java/org/mininuniver/interactivemap/config/WebConfig.java
@@ -30,7 +30,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
---
+++ src/main/java/org/mininuniver/interactivemap/dto/AuthResponse.java
@@ -0,0 +1,10 @@
+package org.mininuniver.interactivemap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+    private final String token;
+}
